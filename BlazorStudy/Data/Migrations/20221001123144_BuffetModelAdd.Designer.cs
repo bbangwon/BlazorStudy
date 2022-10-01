afeb895d637d10a0531de92c1996b53754a054de
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-10-01 21:41:08 +0900

    Candidate Model 추가

--- BlazorStudy/Data/Migrations/20221001123144_BuffetModelAdd.Designer.cs
+++ BlazorStudy/Data/Migrations/20221001123144_BuffetModelAdd.Designer.cs
@@ -4,6 +4,7 @@ using BlazorStudy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorStudy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221001123144_BuffetModelAdd")]
+    partial class BuffetModelAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BlazorStudy/Models/Candidates/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorStudy.Models.Candidates
+{
+    /// <summary>
+    /// 후보자, 지원자(Applicant)
+    /// </summary>
+    public class Candidate
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+        public bool IsEnrollment { get; set; }
+    }
+}
